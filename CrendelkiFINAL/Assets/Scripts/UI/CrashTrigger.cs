2b331712d785cb5ff58a6b8eac8aa84924d604d1
RavshanRashavelly <56175043+RavshanRashavelly@users.noreply.github.com>
2021-12-03 19:59:38 +0300

    Educate

--- CrendelkiFINAL/Assets/Scripts/UI/CrashTrigger.cs
+++ CrendelkiFINAL/Assets/Scripts/UI/CrashTrigger.cs
@@ -10,7 +10,7 @@ public class CrashTrigger : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-       if(other.tag == "Drone")
+       if(other.tag == "let")
        {
             Lose.SetActive(true);
             Time.timeScale = 0f;  
@@ -24,7 +24,7 @@ public class CrashTrigger : MonoBehaviour
 
     public void Again()
     {
-        Application.LoadLevel("EducateLevel");
+        Application.LoadLevel("Test");
         Time.timeScale = 1f;
     }
 }
